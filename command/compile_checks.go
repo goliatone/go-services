2b327ea5a65fd3a359af56880442d9a6584d24b7
goliatone <140582+goliatone@users.noreply.github.com>
2026-02-13 14:38:33 -0800

    add: command compile checks

---
+++ command/compile_checks.go
@@ -0,0 +1,17 @@
+package command
+
+import gocmd "github.com/goliatone/go-command"
+
+var (
+	_ gocmd.Commander[ConnectMessage]            = (*ConnectCommand)(nil)
+	_ gocmd.Commander[StartReconsentMessage]     = (*StartReconsentCommand)(nil)
+	_ gocmd.Commander[CompleteReconsentMessage]  = (*CompleteReconsentCommand)(nil)
+	_ gocmd.Commander[CompleteCallbackMessage]   = (*CompleteCallbackCommand)(nil)
+	_ gocmd.Commander[RefreshMessage]            = (*RefreshCommand)(nil)
+	_ gocmd.Commander[RevokeMessage]             = (*RevokeCommand)(nil)
+	_ gocmd.Commander[InvokeCapabilityMessage]   = (*InvokeCapabilityCommand)(nil)
+	_ gocmd.Commander[SubscribeMessage]          = (*SubscribeCommand)(nil)
+	_ gocmd.Commander[RenewSubscriptionMessage]  = (*RenewSubscriptionCommand)(nil)
+	_ gocmd.Commander[CancelSubscriptionMessage] = (*CancelSubscriptionCommand)(nil)
+	_ gocmd.Commander[AdvanceSyncCursorMessage]  = (*AdvanceSyncCursorCommand)(nil)
+)
