39aa073d704423499491769f33fd2edbd37e39b1
goliatone <140582+goliatone@users.noreply.github.com>
2026-02-15 14:47:27 -0800

    fix: include require callback redirect

--- core/config.go
+++ core/config.go
@@ -9,15 +9,21 @@ type InheritanceConfig struct {
 	EnabledProviders []string `koanf:"enabled_providers" mapstructure:"enabled_providers"`
 }
 
+type OAuthConfig struct {
+	RequireCallbackRedirect bool `koanf:"require_callback_redirect" mapstructure:"require_callback_redirect"`
+}
+
 type Config struct {
 	ServiceName string            `koanf:"service_name" mapstructure:"service_name"`
 	Inheritance InheritanceConfig `koanf:"inheritance" mapstructure:"inheritance"`
+	OAuth       OAuthConfig       `koanf:"oauth" mapstructure:"oauth"`
 }
 
 func DefaultConfig() Config {
 	return Config{
 		ServiceName: "services",
 		Inheritance: InheritanceConfig{},
+		OAuth:       OAuthConfig{},
 	}
 }
 
