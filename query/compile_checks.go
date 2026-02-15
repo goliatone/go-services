b08985985f67e2a6e62668aa5d1effce20d38807
goliatone <140582+goliatone@users.noreply.github.com>
2026-02-15 15:14:55 -0800

    add: new query handlers

--- query/compile_checks.go
+++ query/compile_checks.go
@@ -8,4 +8,6 @@ import (
 var (
 	_ gocmd.Querier[LoadSyncCursorMessage, core.SyncCursor]                 = (*LoadSyncCursorQuery)(nil)
 	_ gocmd.Querier[ListServicesActivityMessage, core.ServicesActivityPage] = (*ListServicesActivityQuery)(nil)
+	_ gocmd.Querier[GetInstallationMessage, core.Installation]              = (*GetInstallationQuery)(nil)
+	_ gocmd.Querier[ListInstallationsMessage, []core.Installation]          = (*ListInstallationsQuery)(nil)
 )
