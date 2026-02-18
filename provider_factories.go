023e4b3641db0923b70bbc79ab6ad575d1b74a3c
goliatone <140582+goliatone@users.noreply.github.com>
2026-02-18 12:59:18 -0800

    add: provider factories

--- provider_factories.go
+++ provider_factories.go
@@ -2,11 +2,18 @@ package services
 
 import (
 	"github.com/goliatone/go-services/core"
+	"github.com/goliatone/go-services/providers/amazon"
 	"github.com/goliatone/go-services/providers/github"
 	"github.com/goliatone/go-services/providers/google/calendar"
 	"github.com/goliatone/go-services/providers/google/docs"
 	"github.com/goliatone/go-services/providers/google/drive"
 	"github.com/goliatone/go-services/providers/google/gmail"
+	"github.com/goliatone/go-services/providers/google/shopping"
+	"github.com/goliatone/go-services/providers/meta/facebook"
+	"github.com/goliatone/go-services/providers/meta/instagram"
+	"github.com/goliatone/go-services/providers/pinterest"
+	"github.com/goliatone/go-services/providers/shopify"
+	"github.com/goliatone/go-services/providers/tiktok"
 )
 
 func GitHubProvider(cfg github.Config) (core.Provider, error) {
@@ -27,4 +34,32 @@ func DocsProvider(cfg docs.Config) (core.Provider, error) {
 
 func CalendarProvider(cfg calendar.Config) (core.Provider, error) {
 	return calendar.New(cfg)
+}
+
+func ShopifyProvider(cfg shopify.Config) (core.Provider, error) {
+	return shopify.New(cfg)
+}
+
+func InstagramProvider(cfg instagram.Config) (core.Provider, error) {
+	return instagram.New(cfg)
+}
+
+func FacebookProvider(cfg facebook.Config) (core.Provider, error) {
+	return facebook.New(cfg)
+}
+
+func TikTokProvider(cfg tiktok.Config) (core.Provider, error) {
+	return tiktok.New(cfg)
+}
+
+func PinterestProvider(cfg pinterest.Config) (core.Provider, error) {
+	return pinterest.New(cfg)
+}
+
+func GoogleShoppingProvider(cfg shopping.Config) (core.Provider, error) {
+	return shopping.New(cfg)
+}
+
+func AmazonProvider(cfg amazon.Config) (core.Provider, error) {
+	return amazon.New(cfg)
 }
